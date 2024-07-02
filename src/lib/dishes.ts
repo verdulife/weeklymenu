e08e8d5d91c91ba8d0d733524b6c9c48a4eeb98d
verdulife <verdu@live.com>
2024-07-02 22:31:33 +0200

    add projects

---
+++ src/lib/dishes.ts
@@ -0,0 +1,122 @@
+export const dishes = [
+  {
+    day: "Lunes",
+    meals: [
+      {
+        type: "cena",
+        name: "Tortilla de patatas y pan con tomate",
+        ingredients: [
+          "patatas",
+          "pan",
+          "tomate",
+          "cebolla",
+          "huevo"
+        ]
+      }
+    ]
+  },
+  {
+    day: "Martes",
+    meals: [
+      {
+        type: "cena",
+        name: "Pollo a la plancha y ensalada de tomates cherris, mezclum y mozzarella",
+        ingredients: [
+          "pollo",
+          "tomates cherri",
+          "mezclum",
+          "mozzarella"
+        ]
+      },
+    ],
+  },
+  {
+    day: "Miércoles",
+    meals: [
+      {
+        type: "cena",
+        name: "Pescado blanco al limon y ensalada de pepino",
+        ingredients: [
+          "pescado",
+          "limón",
+          "pepino",
+          "cebolla"
+        ]
+      }
+    ]
+  },
+  {
+    day: "Jueves",
+    meals: [
+      {
+        type: "cena",
+        name: "Wraps de atún, huevo duro, mayonesa, queso fresco y mezclum",
+        ingredients: [
+          "atún",
+          "huevo",
+          "mayonesa",
+          "queso",
+          "mezclum"
+        ]
+      }
+    ]
+  },
+  {
+    day: "Viernes",
+    meals: [
+      {
+        type: "cena",
+        name: "Pasta/arroz con cebolla, champiñones, ajo, tomate cherry y salsa de soja",
+        ingredients: [
+          "pasta",
+          "cebolla",
+          "champiñones",
+          "ajo",
+          "tomate",
+          "salsa de soja"
+        ]
+      }
+    ]
+  },
+  {
+    day: "Sábado",
+    meals: [
+      {
+        type: "cena",
+        name: "Lasaña boloñesa de lentejas",
+        ingredients: [
+          "lentejas",
+          "cebolla",
+          "zanahoria",
+          "vino tinto",
+          "queso rallado",
+          "salsa de tomate",
+          "planchas de lasaña",
+          "leche"
+        ]
+      }
+    ]
+  },
+  {
+    day: "Domingo",
+    meals: [
+      {
+        type: "cena",
+        name: "Melón con jamón",
+        ingredients: [
+          "melón",
+          "jamón",
+        ]
+      },
+      {
+        type: "cena",
+        name: "Piña a la plancha con bradwurst",
+        ingredients: [
+          "piña",
+          "bradwurst",
+        ]
+      }
+    ]
+  },
+];
+
